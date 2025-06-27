22609ac5cb2477b78c0d06aa3c335ae6f4257e73
Preygle <mohammad.owais2004@gmail.com>
2025-06-27 22:57:56 +0530

    extract and view .tar file (hardcoded name)

--- src/analyzer/dataset_extractor.py
+++ src/analyzer/dataset_extractor.py
@@ -1,23 +1,20 @@
 import os
 import tarfile
 
-# def is_within_directory(directory, target):
-#     abs_dir = os.path.abspath(directory)
-#     abs_target = os.path.abspath(target)
-#     return os.path.commonpath([abs_dir]) == os.path.commonpath([abs_dir, abs_target])
+def safe_extract(tar, path="."):
+    for member in tar.getmembers():
+        member_path = os.path.join(path, member.name)
+    tar.extractall(path)
 
 
-# def safe_extract(tar, path="."):
-#     for member in tar.getmembers():
-#         member_path = os.path.join(path, member.name)
-#         if not is_within_directory(path, member_path):
-#             raise Exception("Path traversal detected in tar file")
-#     tar.extractall(path)
+tar_file = "py150.tar.gz"
 
 
-# with tarfile.open("py150.tar_1") as tar:
-#     safe_extract(tar)
+with tarfile.open(tar_file) as tar:
+    safe_extract(tar)
 
-with open('python50k_eval.json', 'r') as f:
+json_file = "python100k_train.json"
+
+with open(json_file, 'r') as f:
     for _ in range(1):  # Read first 1 lines 
         print(f.readline())
