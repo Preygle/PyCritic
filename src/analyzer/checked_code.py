ae6737e917baa840968fb58571f899ad6636fab4
Preygle <mohammad.owais2004@gmail.com>
2025-07-05 22:09:49 +0530

    corrected code_check.py based on suggestions

--- src/analyzer/checked_code.py
+++ src/analyzer/checked_code.py
@@ -1,11 +1,9 @@
-# Sample code for testing. Contains all possible issues that the analyzer should catch.
+# This file contains corrected code based on the issues identified in the original code snippet (code_check.py).
 
 CONSTANT_VALUE = 42
 
-
 def calculate_sum(number_1,number_2):
     return number_1 + number_2
-
 
 class MyClass:
     def __init__(self):
@@ -15,7 +13,6 @@ class MyClass:
         self.some_value += value
         self.some_value += CONSTANT_VALUE
         return self.some_value
-
 
 def another_bad_function(some_argument):
     print(f"Argument was: {some_argument}")
